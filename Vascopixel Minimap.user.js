2d64bb4852015349bfe432338a5d7d9195102137
Qomorah <48451507+Qomorah@users.noreply.github.com>
2019-03-11 20:58:32 +0200

    Update Vascopixel Minimap.user.js

--- Vascopixel Minimap.user.js
+++ Vascopixel Minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Autumn-Blaze/ponehs/master/';
+window.baseTepmlateUrl = 'https://github.com/Qomorah/suko/tree/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
